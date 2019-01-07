b1a8323d7c980b478f9bd60b2046a43edd8f7226
Frolt <alexanderkamperhaug@hotmail.com>
2019-01-07 11:25:53 +0100

    Initial commit

---
+++ Source/TakeMeHome/TakeMeHome.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TakeMeHome.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TakeMeHome, "TakeMeHome" );
